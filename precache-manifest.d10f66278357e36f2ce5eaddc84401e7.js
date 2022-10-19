3c9c791988622d629e0539216cfc0aff3451aed5
M4RC0N3 <M4RC0N3@users.noreply.github.com>
2022-10-19 17:36:49 +0000

    deploy: 4eb321f372db76996efacecfdf26fc6f56c4f571

---
+++ precache-manifest.d10f66278357e36f2ce5eaddc84401e7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8b9de9b585c0a2199d7",
+    "url": "payment-screen/static/css/main.17d2f4e4.chunk.css"
+  },
+  {
+    "revision": "c8b9de9b585c0a2199d7",
+    "url": "payment-screen/static/js/main.0b49c982.chunk.js"
+  },
+  {
+    "revision": "062ef19ec10ac26fcd17",
+    "url": "payment-screen/static/js/runtime~main.6f44bea8.js"
+  },
+  {
+    "revision": "eb64760c33555bf77531",
+    "url": "payment-screen/static/js/2.538ed1bf.chunk.js"
+  },
+  {
+    "revision": "82c0f64a4efccdd3f18d",
+    "url": "payment-screen/static/js/3.d917a865.chunk.js"
+  },
+  {
+    "revision": "afbb2152aa77e301d16d32bbe2b6ea02",
+    "url": "payment-screen/static/media/american-express.afbb2152.svg"
+  },
+  {
+    "revision": "2da05ecc5dcf8bc25bf9aa832f446f2c",
+    "url": "payment-screen/static/media/aura.2da05ecc.svg"
+  },
+  {
+    "revision": "cca4a6aba5224d02d80127a509628fb9",
+    "url": "payment-screen/static/media/dinners-club.cca4a6ab.svg"
+  },
+  {
+    "revision": "464cbb77bb8be234eced7362a6efb60e",
+    "url": "payment-screen/static/media/discover.464cbb77.svg"
+  },
+  {
+    "revision": "6e0d022b5480007a5fd4bb1a71f89b48",
+    "url": "payment-screen/static/media/elo.6e0d022b.svg"
+  },
+  {
+    "revision": "d6495071689e4d067d2c55a55cb4c821",
+    "url": "payment-screen/static/media/hipercard.d6495071.svg"
+  },
+  {
+    "revision": "e354f092785cf3c297457f500c9a248b",
+    "url": "payment-screen/static/media/jcb.e354f092.svg"
+  },
+  {
+    "revision": "ed5386a570fd4a4d12730b65ab876116",
+    "url": "payment-screen/static/media/mastercard.ed5386a5.svg"
+  },
+  {
+    "revision": "866a7d96ce1da27b06b896a2f5a5ad8c",
+    "url": "payment-screen/static/media/visa.866a7d96.svg"
+  },
+  {
+    "revision": "20d41940068e45b114d32b48d684ef39",
+    "url": "payment-screen/static/media/Raleway-Regular.20d41940.ttf"
+  },
+  {
+    "revision": "87641f9900d717d6bfbf108b8755868e",
+    "url": "payment-screen/static/media/Raleway-SemiBold.87641f99.ttf"
+  },
+  {
+    "revision": "f83c1af3617e13fd1358789e83c2b7ec",
+    "url": "payment-screen/index.html"
+  }
+];
